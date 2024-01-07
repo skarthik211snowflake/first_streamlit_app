8483d760199b187023b35d247f6fa5390c8305e3
skarthik211snowflake <155853599+skarthik211snowflake@users.noreply.github.com>
2024-01-07 18:39:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 
